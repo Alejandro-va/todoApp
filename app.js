8c25a7989775d83e0801d0b04caa4f506b17aa17
alejandro-va <prof.jahz@gmail.com>
2021-02-03 22:40:55 -0300

    primera subida

---
+++ app.js
@@ -0,0 +1,4 @@
+const formulario = document.getElementById('formulario')
+const  listaTareas = document.getElementById('Lista-tareas')
+const templateAlerta = document.getElementById('template-alerta')
+const fragment = new DocumentFragment()
